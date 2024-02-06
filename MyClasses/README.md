ae6ca85b490fc6e1d961df067dbc7e10f7879244
78-quangSky <thien.dangquang.sistrain@gmail.com>
2024-02-06 23:46:22 +0700

    modify setup file

---
+++ MyClasses/README.md
@@ -0,0 +1,8 @@
+# MyClasses Package
+
+The 'MyClasses' package provides a set of classes that can be used for various purposes.
+
+## Installation
+
+To install the 'MyClasses' package, you can use the following command:
+```pip install MyClasses```
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import platform
+
+# List of dependencies to be installed via pip
+install_requires = [
+    'opcua',
+    'pandas',
+    'thread6',
+    'asyncio',
+]
+
+# Linux-specific dependencies to be installed via apt-get
+if platform.system() == 'Linux':
+    install_requires.extend([
+        'python3-numpy',
+        'python3-pandas',
+        # Add other Linux-specific dependencies as needed
+    ])
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='MyClasses',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=install_requires,
+    author='FabLab Innovation',
+    author_email='thien.dangquang.sistrain@gmail.com',
+    description='A package for communicating with PLC via industrial protocols such as OPC UA, Modbus, etc. \
+        and logging data to a CSV file.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
